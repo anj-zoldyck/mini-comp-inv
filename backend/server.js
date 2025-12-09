95e552f94983888d3c0490a886996f905da81272
anj-zoldyck <dizonjelo50@gmail.com>
2025-12-09 14:57:43 +0800

    config server.js & vercel.json

--- backend/server.js
+++ backend/server.js
@@ -9,18 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// MongoDB Connection (Mongoose v7+ syntax)
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error(err));
+  .catch(console.error);
 
-// Routes
 app.use("/api/products", productRoutes);
 
-// Local server CONFIGURED
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
-}
-
-module.exports = app; // export for serverless
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
